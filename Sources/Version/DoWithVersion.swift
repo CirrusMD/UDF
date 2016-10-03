0bc2e49ac76fde659c2c3ed37e8d871d88c2420c
David Nix <davidnix81@gmail.com>
2016-10-03 10:03:40 -0600

    refactor VersionedOperation to just a function
    An NSOperation is overkill.

--- Sources/Version/DoWithVersion.swift
+++ Sources/Version/DoWithVersion.swift
@@ -8,33 +8,16 @@
 
 import Foundation
 
-
-open class VersionedOperation<T>: Operation {
-
-    fileprivate let key: TrackableKey
-    fileprivate let version: Version<T>
-    fileprivate let task: () -> Void
-
-    public init(key: AnyObject, version: Version<T>, task: @escaping () -> Void) {
-        self.key = TrackableKey(key)
-        self.task = task
-        self.version = version
-        super.init()
+public func DoWithVersion<T>(key: AnyObject, version: Version<T>, task: () -> Void) {
+    let trackable = TrackableKey(key)
+    guard trackable.objectRef != nil else {
+        return
     }
-
-    open override func main() {
-        guard !isCancelled && key.objectRef != nil else {
-            return
-        }
-
-        guard !versionExists(key, version: version) else {
-            return
-        }
-
-        task()
+    guard !versionExists(trackable, version: version) else {
+        return
     }
+    task()
 }
-
 
 private struct TrackableKey: Hashable {
     weak var objectRef: AnyObject?
@@ -50,7 +33,7 @@ private func ==(lhs: TrackableKey, rhs: TrackableKey) -> Bool {
     return lhs.hashValue == rhs.hashValue
 }
 
-private let mutex = DispatchQueue(label: "com.cirrusmd.exclusiveOperation", attributes: DispatchQueue.Attributes.concurrent)
+private let mutex = DispatchQueue(label: "com.cirrusmd.UDF.versions", attributes: DispatchQueue.Attributes.concurrent)
 private var HISTORY = [TrackableKey: [Int: Bool]]()
 
 private func versionExists<T>(_ key: TrackableKey, version: Version<T>) -> Bool {
